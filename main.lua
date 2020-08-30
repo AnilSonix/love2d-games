8319283f1f0ea9896d0c7ec23eb6c45684adaf7f
anil soni <anil.sonix@gmail.com>
2020-08-30 18:03:33 +0530

    Adding StateMachine

--- main.lua
+++ main.lua
@@ -1,15 +1,24 @@
+require("src.StateMachine")
+
 function love.load()
+    gStateMachine = StateMachine:new({})
+
+    gStateMachine:change("")
+
     love.keyboard.keysPressed = {}
 end
 
 function love.update(dt)
+    gStateMachine:update(dt)
+
     love.keyboard.keysPressed = {}
 end
 
 function love.draw()
+    gStateMachine:render()
 end
 
-function love.keyPressed(key)
+function love.keypressed(key)
     love.keyboard.keysPressed[key] = true
 end
 
